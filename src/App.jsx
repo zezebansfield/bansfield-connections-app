56ab4e455c41b758054c62b8470a0053d2a680e1
Zainab Bansfield <zainab_bansfield@intuit.com>
2025-11-25 14:35:07 -0800

    update project structure

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import "./App.css";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/bansfield-connections-app">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create" element={<Create />} />
