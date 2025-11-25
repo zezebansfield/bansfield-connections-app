bd2326efaac54a61b5181a809cb0faa34cef600c
Zainab Bansfield <zainab_bansfield@intuit.com>
2025-11-25 13:16:24 -0800

    update path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/bansfield-connections-app/",
+export default defineConfig(({ command }) => {
+  const base = command === "build" ? "/bansfield-connections-app/" : "/";
+
+  return {
+    plugins: [react()],
+    base: base,
+  };
 });
